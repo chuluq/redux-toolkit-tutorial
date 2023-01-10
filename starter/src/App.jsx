5d2326129b2830ff5ac499d9df1f3f5916819e6e
chuluq <chuluq@docheck.id>
2023-01-10 13:45:52 +0700

    Add items from api

--- starter/src/App.jsx
+++ starter/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { calculateTotals } from "./features/cart/cartSlice";
+import { calculateTotals, getCartItems } from "./features/cart/cartSlice";
 
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
@@ -9,12 +9,16 @@ import Modal from "./components/Modal";
 function App() {
   const dispatch = useDispatch();
 
-  const { cartItems } = useSelector((store) => store.cart);
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
   const { isOpen } = useSelector((store) => store.modal);
 
   useEffect(() => {
     dispatch(calculateTotals());
   }, [cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+  }, []);
 
   return (
     <main>
